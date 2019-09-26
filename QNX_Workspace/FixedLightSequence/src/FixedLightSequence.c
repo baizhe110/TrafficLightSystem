83b66ef8e089ba1b53fec48033ddff68b362790c
TheMechatronicGuy <55673456+TheMechatronicGuy@users.noreply.github.com>
2019-09-26 15:22:13 +1000

    Update FixedLightSequence.c

--- QNX_Workspace/FixedLightSequence/src/FixedLightSequence.c
+++ QNX_Workspace/FixedLightSequence/src/FixedLightSequence.c
@@ -4,6 +4,7 @@
 #include <sys/netmgr.h>
 #include <sys/neutrino.h>
 #include <errno.h>
+#include <pthread.h>
 
 
 #define MY_PULSE_CODE   _PULSE_CODE_MINAVAIL
@@ -23,6 +24,10 @@ int                     chid;
 int                     rcvid;
 my_message_t            msg;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  // needs to be set to PTHREAD_MUTEX_INITIALIZER;
+
+char NewCarGlobal[3]="aaa";
+
 enum states { EWR_NSR_EWTR_NSTR_0,
 	EWR_NSR_EWTG_NSTR_1,
 	EWR_NSR_EWTY_NSTR_2,
@@ -38,66 +43,121 @@ enum states { EWR_NSR_EWTR_NSTR_0,
 enum states CurState;
 //--- --- --- ---
 
+
+void *keyboard(void *notused)
+{
+	while(1)
+	{
+		char NewCarKey[3];
+		scanf(" %s",&NewCarKey);
+	pthread_mutex_lock(&mutex);
+	strcpy(NewCarGlobal,NewCarKey);
+	pthread_mutex_unlock(&mutex);
+	printf("%s\n",&NewCarGlobal);
+	}
+}
+
 void Initialise()
 {
-	printf("In state0: EWR-NSR\n");
+	printf("Start of fixed light sequence\n");
 	timer_settime(timer_id, 0, &itime1, NULL);
 	MsgReceive(chid, &msg, sizeof(msg), NULL);
 }
+
 void DoSomething0()
 {
-	printf("In state1: EWR-NSR\n");
+	printf("In state0: EWR_NSR_EWTR_NSTR_0\n");
 	timer_settime(timer_id, 0, &itime1, NULL);
 	MsgReceive(chid, &msg, sizeof(msg), NULL);
 }
+
 void DoSomething1()
 {
-	printf("In state1: EWR-NSR\n");
+	printf("In state1: EWR_NSR_EWTG_NSTR_1\n");
 	timer_settime(timer_id, 0, &itime1, NULL);
 	MsgReceive(chid, &msg, sizeof(msg), NULL);
 }
 void DoSomething2()
 {
-	printf("In state2: EWG-NSR\n");
+	printf("In state2: EWR_NSR_EWTY_NSTR_2\n");
 	timer_settime(timer_id, 0, &itime2, NULL);
 	MsgReceive(chid, &msg, sizeof(msg), NULL);
-
 }
+
 void DoSomething3()
 {
-	printf("In state3: EWY-NSR\n");
-	timer_settime(timer_id, 0, &itime1, NULL);
-	MsgReceive(chid, &msg, sizeof(msg), NULL);
-}
-void DoSomething4()
-{
-	printf("In state4: EWR-NSR\n");
-	timer_settime(timer_id, 0, &itime1, NULL);
-	MsgReceive(chid, &msg, sizeof(msg), NULL);
-}
-void DoSomething5()
-{
-	printf("In state5: EWR-NSG\n");
-	timer_settime(timer_id, 0, &itime2, NULL);
-	MsgReceive(chid, &msg, sizeof(msg), NULL);
-}
-void DoSomething6()
-{
-	printf("In state6: EWY-NSY\n");
+	printf("In state3: EWR_NSR_EWTR_NSTR_3\n");
 	timer_settime(timer_id, 0, &itime1, NULL);
 	MsgReceive(chid, &msg, sizeof(msg), NULL);
 }
 
-enum states SingleStep_TrafficLight_SM(void *CurrentState)
+void DoSomething4()
+{
+	printf("In state4: EWG_NSR_EWTR_NSTR_4\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething5()
+{
+	printf("In state5: EWY_NSR_EWTR_NSTR_5\n");
+	timer_settime(timer_id, 0, &itime2, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething6()
+{
+	printf("In state6: EWR_NSR_EWTR_NSTR_6\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething7()
+{
+	printf("In state7: EWR_NSR_EWTR_NSTG_7\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething8()
+{
+	printf("In state8: EWR_NSR_EWTR_NSTY_8\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething9()
+{
+	printf("In state9: EWR_NSR_EWTR_NSTR_9\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething10()
+{
+	printf("In state10: EWR_NSG_EWTR_NSTR_10\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+void DoSomething11()
+{
+	printf("In state11: EWR_NSY_EWTR_NSTR_11\n");
+	timer_settime(timer_id, 0, &itime1, NULL);
+	MsgReceive(chid, &msg, sizeof(msg), NULL);
+}
+
+enum states FixedLightSequence(void *CurrentState)
+
 {
 
 	enum states CurState = *(enum states*)CurrentState;
 
+
 	//CurState = CurrentState;
 
-initialise();
+	switch (CurState){
 
-	switch (CurState)
 
 	case EWR_NSR_EWTR_NSTR_0:
 		DoSomething0();
@@ -140,26 +200,109 @@ initialise();
 		CurState = EWR_NSG_EWTR_NSTR_10;
 		break;
 	case EWR_NSG_EWTR_NSTR_10:
-		Dosomething10();
+		DoSomething10();
 		CurState = EWR_NSY_EWTR_NSTR_11;
 		break;
 	case EWR_NSY_EWTR_NSTR_11:
 		DoSomething11();
-		CurState = EWR_NSY_EWTR_NSTR_0;
+		CurState = EWR_NSR_EWTR_NSTR_0;
+		break;
 	}
 	return CurState;
 }
 
 
+enum states SensorDrivenLightSequence(void *CurrentState)
+
+{
+
+	enum states CurState = *(enum states*)CurrentState;
 
 
+	//CurState = CurrentState;
+
+	char NewCarReceive[3];
+
+	switch (CurState){
+
+	case EWR_NSR_EWTR_NSTR_0:
+		DoSomething0();
+		pthread_mutex_lock(&mutex);
+		strcpy(NewCarReceive,NewCarGlobal);
+		pthread_mutex_unlock(&mutex);
+//		printf("%s\n",NewCarGlobal);
+//		printf("%d\n",strcmp("ewt",NewCarGlobal));
+		if(strcmp("ewt",NewCarGlobal) != 1)
+		{
+			printf("Car waiting, east west right turn");
+			CurState = EWR_NSR_EWTG_NSTR_1;
+		}
+		else if(strcmp("ew",NewCarGlobal) != 1)
+		{
+			printf("Car waiting, east west");
+			CurState = EWG_NSR_EWTR_NSTR_4;
+		}
+		else
+		{
+			CurState = EWR_NSR_EWTR_NSTR_0;
+		}
+		break;
+	case EWR_NSR_EWTG_NSTR_1:
+		DoSomething1();
+		CurState = EWR_NSR_EWTY_NSTR_2;
+		break;
+	case EWR_NSR_EWTY_NSTR_2:
+		DoSomething2();
+		CurState = EWR_NSR_EWTR_NSTR_3;
+		break;
+	case EWR_NSR_EWTR_NSTR_3:
+		DoSomething3();
+		CurState = EWG_NSR_EWTR_NSTR_4;
+		break;
+	case EWG_NSR_EWTR_NSTR_4:
+		DoSomething4();
+		CurState = EWY_NSR_EWTR_NSTR_5;
+		break;
+	case EWY_NSR_EWTR_NSTR_5:
+		DoSomething5();
+		CurState = EWR_NSR_EWTR_NSTR_6;
+		break;
+	case EWR_NSR_EWTR_NSTR_6:
+		DoSomething6();
+		CurState = EWR_NSR_EWTR_NSTG_7;
+		break;
+	case EWR_NSR_EWTR_NSTG_7:
+		DoSomething7();
+		CurState = EWR_NSR_EWTR_NSTY_8;
+		break;
+	case EWR_NSR_EWTR_NSTY_8:
+		DoSomething8();
+		CurState = EWR_NSR_EWTR_NSTR_9;
+		break;
+	case EWR_NSR_EWTR_NSTR_9:
+		DoSomething9();
+		CurState = EWR_NSG_EWTR_NSTR_10;
+		break;
+	case EWR_NSG_EWTR_NSTR_10:
+		DoSomething10();
+		CurState = EWR_NSY_EWTR_NSTR_11;
+		break;
+	case EWR_NSY_EWTR_NSTR_11:
+		DoSomething11();
+		CurState = EWR_NSR_EWTR_NSTR_0;
+		break;
+	}
+	return CurState;
+}
 
 
 int main(int argc, char *argv[])
 {
 	printf("Fixed Sequence Traffic Lights State Machine\n");
 	int Runtimes=30, counter = 0;
-	enum states CurrentState=0; // Declaring the enum within the main
+	enum states CurrentState=0;
+	int Sequence=1, OldSequence=0;
+
 	// means we will need to pass it by address
 
 		chid = ChannelCreate(0); // Create a communications channel
@@ -201,9 +344,40 @@ int main(int argc, char *argv[])
 		itime2.it_interval.tv_sec = 0;          // 1 second
 		itime2.it_interval.tv_nsec = 0; // 500 million nsecs = .5 secs
 
+
+		pthread_mutex_init(&mutex,NULL);
+		pthread_t  th1;
+		pthread_create(&th1, NULL, keyboard, NULL);
+
+
 	while (counter < Runtimes)
 	{
-		CurrentState = SingleStep_TrafficLight_SM( &CurrentState ); // pass address
+		if(Sequence != OldSequence)
+		{
+			CurrentState = 0;
+			if(Sequence==1)
+				{
+				printf("Start of sensor driven light sequence\n");
+				}
+			else if(Sequence == 0)
+				{
+				printf("Start of fixed light sequence\n");
+				}
+
+		}
+
+		if(Sequence == 0)
+		{
+		CurrentState = FixedLightSequence( &CurrentState ); // pass address
 		counter++;
+		}
+		else if (Sequence == 1)
+		{
+		CurrentState = SensorDrivenLightSequence( &CurrentState ); // pass address
+		counter++;
+		}
+		OldSequence = Sequence;
 	}
+
+
 }
